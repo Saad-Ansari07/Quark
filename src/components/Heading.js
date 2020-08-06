bf83ef73ddddbaee0dca34ec614a8dca43bc5c16
Mohammad Saad Ansari <59654519+Saad-Ansari07@users.noreply.github.com>
2020-08-06 20:11:35 +0500

    Update Heading.js

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -3,7 +3,7 @@ import './Heading.css'
 import useWebAnimations, { fadeInRight } from "@wellyshen/use-web-animations";
 
 export const Heading = () => {
-    const { x } = useWebAnimations({ ...fadeInRight });
+    const { ref: x } = useWebAnimations({ ...fadeInRight });
     return (
         <div className="heading" ref={x}>
             <h1>Explode with charisma! work with Quark <br></br>and 
